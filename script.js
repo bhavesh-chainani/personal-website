52b8e597e4f97c66fb5776351f080cecd69ee8e5
bhavesh-chainani <bhaveshchainani@gmail.com>
2024-08-05 10:57:52 +0800

    Updated initial website.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://api.github.com/users/bhavesh-chainani/repos')
-        .then(response => response.json())
-        .then(data => {
-            const projectsList = document.getElementById('projects-list');
-            data.forEach(repo => {
-                const projectItem = document.createElement('div');
-                projectItem.className = 'project';
-                projectItem.innerHTML = `
-                    <h3>${repo.name}</h3>
-                    <p>${repo.description}</p>
-                    <a href="${repo.html_url}" target="_blank">View on GitHub</a>
-                `;
-                projectsList.appendChild(projectItem);
-            });
-        });
-});
+function toggleMenu() {
+    const menu =document.querySelector(".menu-links"); 
+    const icon =document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function sendMail() {
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    }
+
+    emailjs
+    .send("service_20pigze", "template_jm79tde", params)
+    .then((res) => {
+       document.getElementById("name").value = "";
+       document.getElementById("email").value = "";
+       document.getElementById("message").value = "";
+       console.log(res);
+       alert("Message sent successfully!");
+    })
+    .catch((err) => console.log(err));
+}
